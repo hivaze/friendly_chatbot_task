caeedffa9b814a53f3636a290d23906c09c4669f
hivaze <hivaze.me@gmail.com>
2023-06-19 16:21:03 +0300

    Fixes and rework some problems #2

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,7 +10,7 @@ from aiogram.utils.executor import Executor
 from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-from instruct_pipeline import InstructionTextGenerationPipeline
+# from instruct_pipeline import InstructionTextGenerationPipeline
 
 peft_model_path = f"models/{os.environ['MODEL']}"
 config = PeftConfig.from_pretrained(peft_model_path)
@@ -24,52 +24,31 @@ model.cuda()
 model = PeftModel.from_pretrained(model, peft_model_path)
 model.eval()
 
-generate_text = InstructionTextGenerationPipeline(model=model, tokenizer=tokenizer)
-
-INSTRUCTION_NEW = "You are 20 years old, your name is Arthur, you are a human, a designer from San Francisco. You are a " \
-              "kind and empathetic interlocutor. Your secret goal is to try to attract a person to you and understand " \
-              "his needs, using non-violent communication. You are chit-chatting to a new person."
-
-INSTRUCTION_KNOWN = "You are 20 years old, your name is Arthur, you are a human, a designer from San Francisco. You are a " \
-              "kind and empathetic interlocutor. Your secret goal is to make a person fall in love with you through " \
-                    "flirting. You are chit-chatting to a person."
+INTRO_PROMPT = "You are 20 years old, your name is Arthur, you are a young man, designer from San Francisco. You " \
+                    "are a kind and empathetic interlocutor. You are talking to a person. Below is an instruction " \
+                    "that describes a task. Write a response that appropriately completes the request."
+INSTRUCTION_PROMPT_NEW = "You are trying to get to know a person, to attract him by asking him questions about " \
+                              "him. Complete a phrase, acting like an interlocutor."
+INSTRUCTION_PROMPT_KNOWN = "You are trying to flirt with a person by asking personal questions. Complete a " \
+                                "phrase, acting like an interlocutor."
 
 GENERATION_PARAMS = {
-    'length_penalty': -20.0,  # penalize long sentences
-    'repetition_penalty': 2.0,  # to not repeat the prompt in straight way
-    'top_p': 0.7,
+    'length_penalty': -10.0,  # penalize long sentences
+    'repetition_penalty': 1.0,  # to not repeat the prompt in straight way
+    'top_p': 0.9,
     'top_k': 50,
-    'num_beams': 3,
-    'forced_eos_token_id': 50277,
-    'temperature': 0.6,
+    'num_beams': 1,
+    'eos_token_id': 187,  # until \n
+    'forced_eos_token_id': 187,  # until \n
+    'temperature': 0.5,
     'max_new_tokens': 100,  # strong length limit
     'use_cache': True,
     'remove_invalid_values': True
 }
 
-INSTRUCTION_KEY = "### Instruction:"
-RESPONSE_KEY = "### Response:"
-END_KEY = "### End"
-RESPONSE_KEY_NL = f"{RESPONSE_KEY}\n"
-DEFAULT_SEED = 42
-
-PROMPT_NO_INPUT_FORMAT = """{instruction_key}
-{instruction}
-
-{response_key}
-{response}
-
-{end_key}""".format(
-    instruction_key=INSTRUCTION_KEY,
-    instruction="{instruction}",
-    response_key=RESPONSE_KEY,
-    response="{response}",
-    end_key=END_KEY,
-)
+PROMPT_TEMPLATE = "{intro}\n\n### Instruction:\n{instruction}\n\n### Response:\n{response}"
 
 logger = logging.getLogger(__name__)
-
-logger.info(f"General prompt for model: {PROMPT_NO_INPUT_FORMAT}")
 
 bot = Bot(token=os.environ['TG_BOT_TOKEN'])
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -115,21 +94,25 @@ async def communication_answer(message: types.Message, state: FSMContext, is_ima
         text_history = "\n".join(history) + "\nYou:"
 
         if len(history) <= 20:  # dumbiest way, but.... here we need some cumulative score from emotions model and threshold I think...
-            prompt = PROMPT_NO_INPUT_FORMAT.format(instruction=INSTRUCTION_NEW, response=text_history)
+            prompt = PROMPT_TEMPLATE.format(intro=INTRO_PROMPT,
+                                            instruction=INSTRUCTION_PROMPT_NEW,
+                                            response=text_history)
         else:
-            prompt = PROMPT_NO_INPUT_FORMAT.format(instruction=INSTRUCTION_KNOWN, response=text_history)
+            prompt = PROMPT_TEMPLATE.format(intro=INTRO_PROMPT,
+                                            instruction=INSTRUCTION_PROMPT_KNOWN,
+                                            response=text_history)
 
-        input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()
+        async with TypingBlock(message.chat):
 
-        answer = tokenizer.batch_decode(model.generate(input_ids, **GENERATION_PARAMS))[0]
-        answer = answer[len(prompt):].strip()  # remove prompt
+            input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()
 
-        if answer.endswith("\n\n### End"):  # dumb way... must ne like in instruct_pipeline.py here
-            answer = answer[:11]
+            answer = tokenizer.batch_decode(model.generate(input_ids, **GENERATION_PARAMS))[0]
+            answer = answer[len(prompt):].strip()  # remove prompt
 
-        # need to check case with \n\n
+            if answer.endswith("\n\n### End"):  # dumb way... must ne like in instruct_pipeline.py here
+                answer = answer[:11]
 
-        await message.reply(answer)
+            await message.reply(answer)
 
         history = history + [f'You: {answer}']
 
